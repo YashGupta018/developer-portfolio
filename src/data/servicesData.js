c4a56ad28df2f9ea8727f83e7d26b4938475b201
YashGupta018 <ygupta2001@gmail.com>
2022-11-23 22:49:00 +0530

    comment added

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,7 +1,7 @@
-import { BiShoppingBag, BiPencil } from "react-icons/bi";
+/*import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
 import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
-import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";*/
 
 export const servicesData = [
     /*{
